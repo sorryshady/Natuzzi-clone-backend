21b32e8673f4d1c183ffae175ca8ffc018b50b5f
sorryShady <akhilnishadofficial@gmail.com>
2023-11-04 11:36:26 +0530

    fix: apiError being logged instead of being returned as response

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,12 +3,8 @@ const catchAsync = require('../utils/catchAsync')
 const { userService } = require('../services')
 
 const register = catchAsync(async (req, res) => {
-  try {
-    const user = await userService.createUser(req.body)
-    return res.status(httpStatus.CREATED).json({ user })
-  } catch (error) {
-    console.log(error)
-  }
+  const user = await userService.createUser(req.body)
+  return res.status(httpStatus.CREATED).json({ user })
   // return res.send({
   //   code: httpStatus.OK,
   //   data: req.body,
