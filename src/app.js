a1c08f9d6e7d6e63f4855bd6cbbf65a2f0871042
sorryShady <akhilnishadofficial@gmail.com>
2023-11-05 18:47:36 +0530

    add res.cookie

--- src/app.js
+++ src/app.js
@@ -8,10 +8,13 @@ const ApiError = require('./utils/ApiError')
 const routes = require('./routes/index')
 const passport = require('passport')
 const { jwtStrategy } = require('./configs/passport')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 
 app.use(helmet())
+
+app.use(cookieParser())
 
 app.use(express.json())
 
@@ -19,8 +22,14 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(compression())
 
-app.use(cors())
-app.options('*', cors())
+const corsConfig = {
+  origin: true,
+  credentials: true,
+}
+
+app.use(cors(corsConfig))
+app.options('*', cors(corsConfig))
+
 
 app.use(passport.initialize())
 passport.use('jwt', jwtStrategy)
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,14 +5,26 @@ const { userService, tokenService, authService } = require('../services')
 const register = catchAsync(async (req, res) => {
   const user = await userService.createUser(req.body)
   const tokens = await tokenService.generateAuthTokens(user)
-  return res.status(httpStatus.CREATED).json({ user, tokens })
+  return res
+    .status(httpStatus.CREATED)
+    .cookie('jwt', tokens.access.token, {
+      expires: tokens.access.expires,
+      httpOnly: true,
+    })
+    .json({ user })
 })
 
 const login = catchAsync(async (req, res) => {
   const { email, password, rememberMe } = req.body
   const user = await authService.loginUser(email, password)
   const tokens = await tokenService.generateAuthTokens(user, rememberMe)
-  return res.status(httpStatus.OK).send({ user, tokens })
+  return res
+    .status(httpStatus.OK)
+    .cookie('jwt', tokens.access.token, {
+      expires: tokens.access.expires,
+      httpOnly: true,
+    })
+    .json({ user })
 })
 
 module.exports = {
