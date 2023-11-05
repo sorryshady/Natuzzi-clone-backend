e95ff1e6d2efb9db28e10335e70fa0b1db59c51d
sorryShady <akhilnishadofficial@gmail.com>
2023-11-05 15:42:05 +0530

    add res.cookie to store jwt in cookie

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,14 +5,26 @@ const { userService, tokenService, authService } = require('../services')
 const register = catchAsync(async (req, res) => {
   const user = await userService.createUser(req.body)
   const tokens = await tokenService.generateAuthTokens(user)
-  return res.status(httpStatus.CREATED).json({ user, tokens })
+  const cookieOptions = {
+    expires: tokens.access.expires,
+    // secure: true,
+    httpOnly: true,
+  }
+  res.cookie('jwt', tokens.access.token, cookieOptions)
+  return res.status(httpStatus.CREATED).json({ user })
 })
 
 const login = catchAsync(async (req, res) => {
   const { email, password, rememberMe } = req.body
   const user = await authService.loginUser(email, password)
   const tokens = await tokenService.generateAuthTokens(user, rememberMe)
-  return res.status(httpStatus.OK).send({ user, tokens })
+  const cookieOptions = {
+    expires: tokens.access.expires,
+    // secure: true,
+    httpOnly: true,
+  }
+  res.cookie('jwt', tokens.access.token, cookieOptions)
+  return res.status(httpStatus.OK).send({ user })
 })
 
 module.exports = {
